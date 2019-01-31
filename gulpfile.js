7c47a1346b7f66bbfc891aa75defff77dad2c821
Isaiah Odhner <isaiahodhner@gmail.com>
2019-01-31 15:56:27 -0500

    gulp-plumber doesn't seem to work

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,14 @@
 const gulp = require('gulp')
 const watch = require('gulp-watch')
+const plumber = require('gulp-plumber')
 
 gulp.task('watch', () => {
 	const postcss = require('gulp-postcss')
 	const sourcemaps = require('gulp-sourcemaps')
 
-	return watch('src/**/*.css', { ignoreInitial: false })
+	return gulp.src('src/**/*.css')
+		.pipe( watch('src/**/*.css') )
+		.pipe( plumber() )
 		.pipe( sourcemaps.init() )
 		.pipe( postcss([ require('precss'), require('autoprefixer') ]) )
 		.pipe( sourcemaps.write('.') )
