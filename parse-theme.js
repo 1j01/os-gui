3b58e6481718b2784996f6a4f12bed8c28634cec
Isaiah Odhner <isaiahodhner@gmail.com>
2020-03-11 14:20:03 -0400

    No transform!

--- parse-theme.js
+++ parse-theme.js
@@ -112,10 +112,14 @@ function renderThemeGraphics(cssProperties) {
 		var scale = 32;
 		var slice_size = border_size * scale;
 		var view_size = base_size * scale;
+		// transform causes janky buggy garbage
+		// var svg = `<svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="${view_size}px" height="${view_size}px" viewBox="0 0 ${view_size} ${view_size}">
+		// 	<g transform="scale(${scale})">
+		// 		${svg_contents}
+		// 	</g>
+		// </svg>`;
 		var svg = `<svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="${view_size}px" height="${view_size}px" viewBox="0 0 ${view_size} ${view_size}">
-			<g transform="scale(${scale})">
-				${svg_contents}
-			</g>
+			${svg_contents.replace(/d="[^"]*"/g, (d)=> d.replace(/\d+/g, (n)=> n * scale))}
 		</svg>`;
 		var url = `data:image/svg+xml,${encodeURIComponent(svg)}`;
 		return `url("${url}") ${slice_size} / ${border_size}px`;
