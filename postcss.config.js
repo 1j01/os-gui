4eabb0bfa9a551ad934fc47dea9fa22bf0471243
Isaiah Odhner <isaiahodhner@gmail.com>
2020-03-10 23:06:55 -0400

    Inline all images in CSS build

---
+++ postcss.config.js
@@ -0,0 +1,10 @@
+module.exports = ctx => ({
+	map: ctx.options.map,
+	parser: ctx.options.parser,
+	plugins: {
+		precss: true,
+		autoprefixer: true,
+		"postcss-url": {url: "inline"},
+		// cssnano: ctx.env === "production" ? {} : false
+	}
+})
