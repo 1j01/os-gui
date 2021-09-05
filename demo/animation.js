29ae63360cd3d5befa6f049a96db3bb3524c0da4
Isaiah Odhner <isaiahodhner@gmail.com>
2021-09-05 17:42:14 -0400

    [demo] Okay that's a basic strange loop effect for the animation working

--- demo/animation.js
+++ demo/animation.js
@@ -14,29 +14,36 @@ for (let i = 0; i < n_$windows; ++i) {
 }
 const animate = () => {
 	requestAnimationFrame(animate);
-	const motherRect = $mothership[0].getBoundingClientRect();
+	const motherRect = $mothership.$content[0].getBoundingClientRect();
 	for (let i = 0; i < $windows.length; i++) {
 		const $window = $windows[i];
 		let x = Math.sin(Date.now() / 1000 + i / n_$windows * Math.PI * 2) * 300;
 		let y = 0;//Math.cos(Date.now() / 1000 + i / n_$windows * Math.PI * 2) * 100;
-		const width = ~~(Math.cos(Date.now() / 1000 + i / n_$windows * Math.PI * 2) * 200);
-		const height = width;
-		x = ~~(x + (window.innerWidth - width) / 2);
-		y = ~~(y + (window.innerHeight - height) / 2);
-		x += motherRect.width / 2;
-		const z = ~~((Math.cos(Date.now() / 1000 + i / n_$windows * Math.PI * 2) + 1) * 100);
+		let width = Math.cos(Date.now() / 1000 + i / n_$windows * Math.PI * 2) * 50 + 100;
+		let height = width;
+		x += (window.innerWidth - width) / 2;
+		y += (window.innerHeight - height) / 2;
+		// x += motherRect.width / 2;
+		// x += motherRect.left + motherRect.width / 2;
+		// y += motherRect.top + motherRect.height / 2;
+		x = ~~x;
+		y = ~~y;
+		width = ~~width;
+		height = ~~height;
+		// const shouldClip = Math.cos(Date.now() / 1000 + i / n_$windows * Math.PI * 2 + Math.PI / 2) > 0;
+		const shouldClip = x - (motherRect.left + motherRect.width / 2) < 0;
 		$window.css({
 			left: x,
 			top: y,
 			width: width,
 			height: height,
-			zIndex: z,
-			clipPath: z > n_$windows * 3/4 ? "" : `polygon(
+			zIndex: ~~((Math.cos(Date.now() / 1000 + i / n_$windows * Math.PI * 2)) * 100),
+			clipPath: shouldClip ? `polygon(
 				${motherRect.left - x}px ${motherRect.top - y}px,
 				${motherRect.right - x}px ${motherRect.top - y}px,
 				${motherRect.right - x}px ${motherRect.bottom - y}px,
 				${motherRect.left - x}px ${motherRect.bottom - y}px
-			)`,
+			)` : '',
 			// clip: `rect(${motherRect.top - x}px, ${motherRect.right - x}px, ${motherRect.bottom - y}px, ${motherRect.left - y}px)`,
 		});
 	}
