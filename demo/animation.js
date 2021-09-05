9be490cbb7895d46fb62ef0f48ca3c3261ef46b3
Isaiah Odhner <isaiahodhner@gmail.com>
2021-09-05 17:29:22 -0400

    [demo] WIP mothership animation

--- demo/animation.js
+++ demo/animation.js
@@ -1,5 +1,10 @@
 const $windows = [];
 const n_$windows = 10;
+const $mothership = new $Window({
+	title: 'Mothership',
+	outerWidth: 400,
+	outerHeight: 300,
+});
 
 for (let i = 0; i < n_$windows; ++i) {
 	const $window = new $Window({
@@ -9,15 +14,30 @@ for (let i = 0; i < n_$windows; ++i) {
 }
 const animate = () => {
 	requestAnimationFrame(animate);
-	const centerX = window.innerWidth / 2;
-	const centerY = window.innerHeight / 2;
+	const motherRect = $mothership[0].getBoundingClientRect();
 	for (let i = 0; i < $windows.length; i++) {
 		const $window = $windows[i];
+		let x = Math.sin(Date.now() / 1000 + i / n_$windows * Math.PI * 2) * 300;
+		let y = 0;//Math.cos(Date.now() / 1000 + i / n_$windows * Math.PI * 2) * 100;
+		const width = ~~(Math.cos(Date.now() / 1000 + i / n_$windows * Math.PI * 2) * 200);
+		const height = width;
+		x = ~~(x + (window.innerWidth - width) / 2);
+		y = ~~(y + (window.innerHeight - height) / 2);
+		x += motherRect.width / 2;
+		const z = ~~((Math.cos(Date.now() / 1000 + i / n_$windows * Math.PI * 2) + 1) * 100);
 		$window.css({
-			left: ~~(Math.sin(Date.now() / 1000 + i / n_$windows * Math.PI) * 100 + centerX),
-			top: ~~(Math.cos(Date.now() / 1000 + i / n_$windows * Math.PI) * 100 + centerY),
-			width: ~~(Math.sin(Date.now() / 1000 + i) * 100 + 100),
-			height: ~~(Math.cos(Date.now() / 1000 + i) * 100 + 100),
+			left: x,
+			top: y,
+			width: width,
+			height: height,
+			zIndex: z,
+			clipPath: z > n_$windows * 3/4 ? "" : `polygon(
+				${motherRect.left - x}px ${motherRect.top - y}px,
+				${motherRect.right - x}px ${motherRect.top - y}px,
+				${motherRect.right - x}px ${motherRect.bottom - y}px,
+				${motherRect.left - x}px ${motherRect.bottom - y}px
+			)`,
+			// clip: `rect(${motherRect.top - x}px, ${motherRect.right - x}px, ${motherRect.bottom - y}px, ${motherRect.left - y}px)`,
 		});
 	}
 };
