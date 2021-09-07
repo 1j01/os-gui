bb8b5e4f7053f6f3b824cea9324ee0e220e6d2ba
Isaiah Odhner <isaiahodhner@gmail.com>
2021-09-07 17:09:54 -0400

    [demo] Clean up, simplify, comment code, and fix remaining scroll jank
    I wanted to have the dynamic content define its own space in the page, but with scroll jank issues manifesting when loading the page and even when dragging/resizing a window the first time after page load in Firefox when the page is loaded scrolled down and zoomed in, I see now that it's not worth it. It was also a lot of code once I tried to work around these issues, and I basically ended up statically defining heights of things anyways (what I wanted to avoid) - just via code instead of directly. This also clarifies the code a lot. So does adding comments to sections of the demo code.

--- demo/demo.js
+++ demo/demo.js
@@ -63,9 +63,11 @@ const menus = {
 };
 // wait for page load (could alternatively just move the script so it executes after the elements are declared)
 $(() => {
+	// Create menu bar
 	const $menubar = new $MenuBar(menus);
 	$menubar.appendTo("#menubar-example");
 
+	// Create demo windows
 	const $app_window_1 = new $Window({ title: "Application Window", resizable: true });
 	$app_window_1.$content.append($("#app-window-example-content").attr("hidden", null));
 
@@ -99,12 +101,27 @@ $(() => {
 	const $tool_window_2 = new $Window({ title: "Tool Window", toolWindow: true, parentWindow: $app_window_2 });
 	$tool_window_2.$content.text("This tool window is a child of the app window.");
 
-	// Note: the windows are positioned later on to fit into the page layout. See below.
+	// Position the windows within the demo page, in the flow of text, but freely moveable
+	const window_list = [
+		[$app_window_1, "app-window-example"],
+		[$tool_window_1, "tool-window-example"],
+		[$app_window_2, "app-window-2-positioner"],
+		[$tool_window_2, "tool-window-2-positioner"],
+	];
+	for (const [$window, positioning_el_id] of window_list) {
+		const $positioning_el = $(`#${positioning_el_id}`);
+		$window.offset({
+			left: $positioning_el.offset().left,
+			top: $positioning_el.offset().top
+		});
+	}
 
+	// Handle toggle buttons
 	$("button.toggle").on("click", (e) => {
 		$(e.target).toggleClass("selected");
 	});
 
+	// Load themes on drag and drop (.theme/.themepack files)
 	async function loadThemeFile(file) {
 		const fileText = await file.text();
 		const cssProperties = parseThemeFileString(fileText);
@@ -132,36 +149,7 @@ $(() => {
 		}
 	});
 
-	/*applyCSSProperties(parseThemeFileString(`
-
-[Control Panel\\Colors]
-ActiveTitle=0 0 128
-Background=0 128 128
-Hilight=0 0 128
-HilightText=255 255 255
-TitleText=255 255 255
-Window=255 255 255
-WindowText=0 0 0
-Scrollbar=192 192 192
-InactiveTitle=128 128 128
-Menu=192 192 192
-WindowFrame=0 0 0
-MenuText=0 0 0
-ActiveBorder=192 192 192
-InactiveBorder=192 192 192
-AppWorkspace=128 128 128
-ButtonFace=192 192 192
-ButtonShadow=128 128 128
-GrayText=128 128 128
-ButtonText=0 0 0
-InactiveTitleText=192 192 192
-ButtonHilight=255 255 255
-ButtonDkShadow=0 0 0
-ButtonLight=192 192 192
-InfoText=0 0 0
-InfoWindow=255 255 225
-
-`));*/
+	// Generate variations of scrollbars
 	const $scrollbar_buttons = $(".scrollbar-demo");
 	$scrollbar_buttons.after(
 		$scrollbar_buttons.clone().css("--scrollbar-size", "15px"),
@@ -172,46 +160,4 @@ InfoWindow=255 255 225
 	$(".scrollbar-demo").each((index, element) => {
 		applyCSSProperties(renderThemeGraphics(getComputedStyle(element)), element);
 	});
-
-	// position the windows within the demo page, in the flow of text, but freely moveable
-	const window_list = [
-		[$app_window_1, "app-window-example"],
-		[$tool_window_1, "tool-window-example"],
-		[$app_window_2, "app-window-2-positioner"],
-		[$tool_window_2, "tool-window-2-positioner"],
-	];
-	for (const [$window, positioning_el_id] of window_list) {
-		const $positioning_el = $(`#${positioning_el_id}`);
-		$positioning_el.width($window.outerWidth());
-		$positioning_el.height($window.outerHeight());
-	}
-	for (const [$window, positioning_el_id] of window_list) {
-		const $positioning_el = $(`#${positioning_el_id}`);
-		$window.offset({
-			left: $positioning_el.offset().left,
-			top: $positioning_el.offset().top
-		});
-
-		// $window.appendTo($positioning_el);
-		// $window.css({
-		// 	position: "relative",
-		// 	top: 0,
-		// 	left: 0
-		// });
-	}
-
-	// Users of library: DON'T WORRY ABOUT THIS STUFF
-	// This is just for the demo page, to prevent scroll jank when loading the page.
-	// This is only needed because of all the dynamic content that is interspersed with text.
-	// Usually you would have floating windows, that aren't trying to also fit into a document.
-	let static_styles = "";
-	for (selector of window_list.map(([, el_id]) => `#${el_id}`).concat([
-		"#scrollbar-demos",
-		"#menubar-example",
-	])) {
-		const el = $(selector)[0];
-		static_styles += `${selector} { width: ${el.offsetWidth}px; height: ${el.offsetHeight}px; }\n`;
-	}
-	window.static_styles = static_styles; // export these static styles to the demo's HTML (manually)
-
 });
