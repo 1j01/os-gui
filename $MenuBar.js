f03fe283c3e154eaaa9be407a00405c0744be40e
Isaiah Odhner <isaiahodhner@gmail.com>
2021-10-14 18:35:17 -0400

    Robustify and simplify RTL handling

--- $MenuBar.js
+++ $MenuBar.js
@@ -288,11 +288,6 @@ function MenuBar(menus) {
 				if (item.submenu) {
 					item_el.classList.add("has-submenu");
 					submenu_area_el.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="10" height="11" viewBox="0 0 10 11" style="fill:currentColor;display:inline-block;vertical-align:middle"><path d="M7.5 4.33L0 8.66L0 0z"/></svg>';
-					setTimeout(() => { // allow time for the menu to be added to the DOM so it can inherit `direction` CSS property if applicable
-						if (get_direction() === "rtl") {
-							submenu_area_el.querySelector("svg").style.transform = "scaleX(-1)";
-						}
-					}, 0);
 
 					const submenu_popup = MenuPopup(item.submenu);
 					submenu_popup_el = submenu_popup.element;
@@ -307,6 +302,7 @@ function MenuBar(menus) {
 						submenu_popup_el.style.display = "";
 						submenu_popup_el.style.zIndex = get_new_menu_z_index();
 						submenu_popup_el.setAttribute("dir", get_direction());
+						submenu_area_el.querySelector("svg").style.transform = get_direction() === "rtl" ? "scaleX(-1)" : "";
 						
 						// console.log("open_submenu — submenu_popup_el.style.zIndex", submenu_popup_el.style.zIndex, "$Window.Z_INDEX", $Window.Z_INDEX, "menus_el.closest('.window').style.zIndex", menus_el.closest(".window").style.zIndex);
 						// setTimeout(() => { console.log("after timeout, menus_el.closest('.window').style.zIndex", menus_el.closest(".window").style.zIndex); }, 0);
