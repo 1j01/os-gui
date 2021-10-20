93738a38fe9f9846a656e498f2cb33e66b0120f9
Isaiah Odhner <isaiahodhner@gmail.com>
2021-10-20 19:23:20 -0400

    WIP: cursor theming hack

--- $Window.js
+++ $Window.js
@@ -1075,4 +1075,70 @@ function $FormWindow(title) {
 exports.$Window = $Window;
 exports.$FormWindow = $FormWindow;
 
+exports.enableCursorTheming = () => {
+	let last_cursor_el = null;
+	let original_cursor = null;
+	let debug_rect_els = [];
+	// TODO: also handle scroll, elements being removed, etc.
+	addEventListener("mousemove", (e) => {
+		for (const debug_rect_el of debug_rect_els) {
+			debug_rect_el.remove();
+		}
+		// TODO: optimize for non-"auto" cursors
+		// if (e.target === last_cursor_el) {
+		// 	return;
+		// }
+		if (last_cursor_el) {
+			last_cursor_el.style.cursor = original_cursor;
+		}
+		last_cursor_el = e.target;
+		original_cursor = e.target.style.cursor; // not computed style, so it resets naturally
+		const computed_style = getComputedStyle(e.target);
+		let cursor = computed_style.cursor;
+		// console.log(cursor);
+
+		if (cursor.startsWith("url")) {
+			return;
+		}
+		
+		if (cursor === "auto") {
+			if (e.target.tagName === "A" && e.target.href) {
+				cursor = "pointer";
+			} else if (computed_style.userSelect === "none") {
+				cursor = "default";
+			} else {
+				// may be "text" or "default"
+				const rects = e.target.getClientRects();
+				for (const rect of rects) {
+					const inside = rect.left <= e.clientX && e.clientX <= rect.right && rect.top <= e.clientY && e.clientY <= rect.bottom;
+					if (inside) {
+						cursor = "text";
+						// break;
+					}
+					const debug_rect_el = E("div");
+					debug_rect_el.style.position = "absolute";
+					debug_rect_el.style.left = `${rect.left}px`;
+					debug_rect_el.style.top = `${rect.top}px`;
+					debug_rect_el.style.width = `${rect.width}px`;
+					debug_rect_el.style.height = `${rect.height}px`;
+					debug_rect_el.style.border = `3px dashed ${inside ? "lime" : "red"}`;
+					debug_rect_el.style.boxSizing = "border-box";
+					debug_rect_el.style.pointerEvents = "none";
+					debug_rect_el.style.zIndex = 1000000;
+					document.body.appendChild(debug_rect_el);
+					debug_rect_els.push(debug_rect_el);
+				}
+				if (cursor === "auto") {
+					cursor = "default";
+				}
+			}
+			console.log("auto ->", cursor);
+		}
+
+		cursor = `var(--cursor-${cursor})`;
+	});
+};
+
+exports.enableCursorTheming();
+
 })(window);
