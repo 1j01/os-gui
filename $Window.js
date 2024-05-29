7a995936d97abf90ba3890abccf05b34d22ab828
Isaiah Odhner <isaiahodhner@gmail.com>
2024-05-29 14:44:31 -0400

    Remove weird event listener removal
    There is no onscroll function defined.
    There is only potentially the global window.onscroll handler,
    which doesn't make sense to remove with jQuery.
    
    Probably there was an onscroll function at some point during
    development, but it wasn't present when this code was added, in:
    https://github.com/1j01/os-gui/commit/5ff6685841f

--- $Window.js
+++ $Window.js
@@ -1516,7 +1516,6 @@ You can also disable this warning by passing {iframes: {ignoreCrossOrigin: true}
 				const pointerId = e.pointerId ?? e.originalEvent?.pointerId; // originalEvent doesn't exist for triggerHandler()
 				if (pointerId !== resize_pointer_id && pointerId !== undefined) { return; } // (allowing synthetic events to affect the drag without pointerId)
 				$G.off("pointermove", handle_pointermove);
-				$G.off("scroll", onscroll);
 				$("body").removeClass("dragging");
 				$G.off("pointerup pointercancel", end_resize);
 				$w.bringTitleBarInBounds();
