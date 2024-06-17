defc10b1296b7d9787208928b3594ab5e6d36ad9
Isaiah Odhner <isaiahodhner@gmail.com>
2024-06-17 01:10:55 -0400

    rm \n

--- tests/windows.spec.ts
+++ tests/windows.spec.ts
@@ -65,8 +65,7 @@ test.describe('$Window Component', () => {
 				closeButton: true
 			});
 		});
-		await page.locator(
-			'.window-minimize-button').click();
+		await page.locator('.window-minimize-button').click();
 		// These exact values may not be perfectly accurate to Windows 98, but it's easier to test exact values than proximity.
 		await expect(page.locator('.window')).toHaveCSS('bottom', '-3px'); // result of `calc(100% - ${titlebar_height + 5}px)`
 		await expect(page.locator('.window')).toHaveCSS('left', '10px'); // hardcoded `spacing`
@@ -77,12 +76,9 @@ test.describe('$Window Component', () => {
 				minimizeButton: true
 			});
 		});
-		await page.locator(
-			'.window-minimize-button').last().click();
-		await expect(page.locator(
-			'.window').last()).toHaveCSS('bottom', '-3px');
-		await expect(page.locator(
-			'.window').last()).toHaveCSS('left', '170px'); // result of `spacing` + `to_width` + `spacing`
+		await page.locator('.window-minimize-button').last().click();
+		await expect(page.locator('.window').last()).toHaveCSS('bottom', '-3px');
+		await expect(page.locator('.window').last()).toHaveCSS('left', '170px'); // result of `spacing` + `to_width` + `spacing`
 
 		await page.evaluate(() => {
 			$Window({
@@ -92,12 +88,9 @@ test.describe('$Window Component', () => {
 			$window.restore(); // free up slot (closing should also do this, tested elsewhere)
 		});
 		await expect(page.locator('.window')).toHaveCount(3);
-		await page.locator(
-			'.window-minimize-button').last().click();
-		await expect(page.locator(
-			'.window').last()).toHaveCSS('bottom', '-3px');
-		await expect(page.locator(
-			'.window').last()).toHaveCSS('left', '10px');
+		await page.locator('.window-minimize-button').last().click();
+		await expect(page.locator('.window').last()).toHaveCSS('bottom', '-3px');
+		await expect(page.locator('.window').last()).toHaveCSS('left', '10px');
 	});
 
 	test('can be dragged by the title bar', async ({ page }) => {
@@ -110,18 +103,13 @@ test.describe('$Window Component', () => {
 			});
 			$window.$content.append('<p>Drag me!</p>').css("padding", "30px");
 		});
-		await page.locator(
-			'.window-titlebar').dispatchEvent('pointerdown', { which: 1 });
-		await page.locator(
-			'.window-titlebar').dispatchEvent('pointermove', { clientX: 0, clientY: 0 });
-		await expect(page.locator(
-			'.window')).toHaveCSS('left', /.*/);
+		await page.locator('.window-titlebar').dispatchEvent('pointerdown', { which: 1 });
+		await page.locator('.window-titlebar').dispatchEvent('pointermove', { clientX: 0, clientY: 0 });
+		await expect(page.locator('.window')).toHaveCSS('left', /.*/);
 		await expect.poll(async () => page.locator('.window').evaluateAll((elements, match) => { const matches = new Set(document.querySelectorAll(match)); return !!elements.find((e) => matches.has(e)); }, /^-?\d+px$/)).toBeTruthy();
-		await expect(page.locator(
-			'.window')).toHaveCSS('top', /.*/);
+		await expect(page.locator('.window')).toHaveCSS('top', /.*/);
 		await expect.poll(async () => page.locator('.window').evaluateAll((elements, match) => { const matches = new Set(document.querySelectorAll(match)); return !!elements.find((e) => matches.has(e)); }, /^-?\d+px$/)).toBeTruthy();
-		await page.locator(
-			'.window-titlebar').dispatchEvent('pointerup');
+		await page.locator('.window-titlebar').dispatchEvent('pointerup');
 		// It should then snap such that you can still reach the title bar
 		// TODO: test horizontal clamping (vertical is easier since it should stop at zero, whereas horizontally it can go off screen _partially_)
 		await expect(page.locator('.window')).toHaveCSS('top', '0px');
@@ -140,10 +128,8 @@ test.describe('$Window Component', () => {
 		});
 		// Maximize
 		await page.locator('.window-titlebar').dblclick();
-		await expect(page.locator(
-			'.window')).toHaveCSS('top', '0px');
-		await expect(page.locator(
-			'.window')).toHaveCSS('left', '0px');
+		await expect(page.locator('.window')).toHaveCSS('top', '0px');
+		await expect(page.locator('.window')).toHaveCSS('left', '0px');
 		// FIXME: weird scrollbar logic (the window itself is causing a scrollbar (though not when maximized) and it's reserving space for it.
 		// I could make it move the window offscreen to the left/top before detecting the scrollbar width, but that would only mitigate the issue.
 		// Is there a way I can make it account for the scrollbar dynamically, rather than detecting the scrollbar width at one point in time?
@@ -152,26 +138,19 @@ test.describe('$Window Component', () => {
 
 		// Try dragging the maximized window
 		await page.locator('.window-titlebar').dispatchEvent('pointerdown', { which: 1 });
-		await page.locator(
-			'.window-titlebar').dispatchEvent('pointermove', { clientX: 50, clientY: 50 });
-		await expect(page.locator(
-			'.window')).toHaveCSS('top', '0px');
-		await expect(page.locator(
-			'.window')).toHaveCSS('left', '0px');
+		await page.locator('.window-titlebar').dispatchEvent('pointermove', { clientX: 50, clientY: 50 });
+		await expect(page.locator('.window')).toHaveCSS('top', '0px');
+		await expect(page.locator('.window')).toHaveCSS('left', '0px');
 		// cy.get('.window').should('have.css', 'width', '300px'); // see above
 		await expect(page.locator('.window')).toHaveCSS('height', '300px');
 
 		// Restore
 		// TODO: test rectangle matches original window rectangle
 		await page.locator('.window-titlebar').dblclick();
-		await expect(page.locator(
-			'.window')).not.toHaveCSS('top', '0px');
-		await expect(page.locator(
-			'.window')).not.toHaveCSS('left', '0px');
-		await expect(page.locator(
-			'.window')).not.toHaveCSS('width', '300px');
-		await expect(page.locator(
-			'.window')).not.toHaveCSS('height', '300px');
+		await expect(page.locator('.window')).not.toHaveCSS('top', '0px');
+		await expect(page.locator('.window')).not.toHaveCSS('left', '0px');
+		await expect(page.locator('.window')).not.toHaveCSS('width', '300px');
+		await expect(page.locator('.window')).not.toHaveCSS('height', '300px');
 
 	});
 
@@ -186,31 +165,21 @@ test.describe('$Window Component', () => {
 			});
 			$window.$content.append('<p>Maximize button test window</p>').css("padding", "30px");
 		});
-		await expect(page.locator(
-			'.window-maximize-button')).toHaveClass(/window-action-maximize/);
-		await page.locator(
-			'.window-maximize-button').click();
-		await expect(page.locator(
-			'.window')).toHaveCSS('top', '0px');
-		await expect(page.locator(
-			'.window')).toHaveCSS('left', '0px');
+		await expect(page.locator('.window-maximize-button')).toHaveClass(/window-action-maximize/);
+		await page.locator('.window-maximize-button').click();
+		await expect(page.locator('.window')).toHaveCSS('top', '0px');
+		await expect(page.locator('.window')).toHaveCSS('left', '0px');
 		// cy.get('.window').should('have.css', 'width', '300px'); // see above
 		await expect(page.locator('.window')).toHaveCSS('height', '300px');
-		await expect(page.locator(
-			'.window-maximize-button')).toHaveClass(/window-action-restore/);
+		await expect(page.locator('.window-maximize-button')).toHaveClass(/window-action-restore/);
 
 		// Restore
 		await page.locator('.window-maximize-button').click();
-		await expect(page.locator(
-			'.window')).not.toHaveCSS('top', '0px');
-		await expect(page.locator(
-			'.window')).not.toHaveCSS('left', '0px');
-		await expect(page.locator(
-			'.window')).not.toHaveCSS('width', '300px');
-		await expect(page.locator(
-			'.window')).not.toHaveCSS('height', '300px');
-		await expect(page.locator(
-			'.window-maximize-button')).toHaveClass(/window-action-maximize/);
+		await expect(page.locator('.window')).not.toHaveCSS('top', '0px');
+		await expect(page.locator('.window')).not.toHaveCSS('left', '0px');
+		await expect(page.locator('.window')).not.toHaveCSS('width', '300px');
+		await expect(page.locator('.window')).not.toHaveCSS('height', '300px');
+		await expect(page.locator('.window-maximize-button')).toHaveClass(/window-action-maximize/);
 	});
 
 	test('can be closed by clicking the close button', async ({ page }) => {
@@ -220,10 +189,8 @@ test.describe('$Window Component', () => {
 			});
 			$window.$content.append('<p>Close me!</p>').css("padding", "30px");
 		});
-		await page.locator(
-			'.window-close-button').click();
-		await expect(page.locator(
-			'.window')).not.toBeVisible();
+		await page.locator('.window-close-button').click();
+		await expect(page.locator('.window')).not.toBeVisible();
 	});
 
 	test('can be resized horizontally by dragging the left edge', async ({ page }) => {
@@ -265,19 +232,14 @@ test.describe('$Window Component', () => {
 					title: 'Test Window'
 				});
 			});
-			await expect(page.locator(
-				'.window-title')).toHaveText('Test Window');
+			await expect(page.locator('.window-title')).toHaveText('Test Window');
 
 			$window.title('New Title');
-			await expect(page.locator(
-
-				'.window-title')).toHaveText('New Title');
+			await expect(page.locator('.window-title')).toHaveText('New Title');
 
 			// @ts-ignore
 			$window.title(420);
-			await expect(page.locator(
-
-				'.window-title')).toHaveText('420');
+			await expect(page.locator('.window-title')).toHaveText('420');
 		});
 		test('should clear the title if given an empty string', async ({ page }) => {
 			await page.evaluate(() => {
@@ -285,13 +247,10 @@ test.describe('$Window Component', () => {
 					title: 'Test Window'
 				});
 			});
-			await expect(page.locator(
-				'.window-title')).toHaveText('Test Window');
+			await expect(page.locator('.window-title')).toHaveText('Test Window');
 
 			$window.title('');
-			await expect(page.locator(
-
-				'.window-title')).toHaveText('');
+			await expect(page.locator('.window-title')).toHaveText('');
 		});
 		test('should return the current title if called without arguments', async ({ page }) => {
 			await page.evaluate(() => {
@@ -435,26 +394,20 @@ test.describe('$Window Component', () => {
 				});
 				$window.setMenuBar(menu);
 			});
-			await expect(page.locator(
-				'[role="menubar"]')).toBeVisible();
+			await expect(page.locator('[role="menubar"]')).toBeVisible();
 			$window.minimize();
-			await expect(page.locator(
-
-				'.window-titlebar')).toHaveCount(1); // wait for titlebar animation to finish
+			await expect(page.locator('.window-titlebar')).toHaveCount(1); // wait for titlebar animation to finish
 			// move the window so the menu bar is visible if it's broken
 			await page.locator('.window-titlebar').dispatchEvent('pointerdown', { which: 1 });
-			await page.locator(
-				'.window-titlebar').dispatchEvent('pointermove', { clientX: 150, clientY: 150 });
-			await page.locator(
-				'.window-titlebar').dispatchEvent('pointerup');
+			await page.locator('.window-titlebar').dispatchEvent('pointermove', { clientX: 150, clientY: 150 });
+			await page.locator('.window-titlebar').dispatchEvent('pointerup');
 			// not a strong enough assertion, since it considers offscreen elements hidden
 			// (you can test with `Cypress.dom.isVisible(document.querySelector("[role=menubar]"))` in the console)
 			await expect(page.locator('[role="menubar"]')).not.toBeVisible();
 			// stronger assertion
 			await expect(page.locator('[role="menubar"]')).toHaveCSS('display', 'none');
 			$window.restore();
-			await expect(page.locator(
-				'[role="menubar"]')).toBeVisible();
+			await expect(page.locator('[role="menubar"]')).toBeVisible();
 		});
 		test('should set up the correct keyboard scope', async ({ page }) => {
 			let activated_menu_item = false;
@@ -479,8 +432,7 @@ test.describe('$Window Component', () => {
 			// works while window is focused
 			await page.locator('.window-content').click();
 			await page.keyboard.down("Alt");
-			await page.locator(
-				'body').fill("t");
+			await page.locator('body').fill("t");
 			await page.keyboard.up("Alt");
 			await page.locator('body').press("Enter");
 			// Can't use cy.wrap(activated_menu_item).should('be.true') because it would be synchronously accessing the value before commands are run
@@ -493,8 +445,7 @@ test.describe('$Window Component', () => {
 			// does nothing while window is not focused
 			await page.locator('body').click();
 			await page.keyboard.down("Alt");
-			await page.locator(
-				'body').fill("t");
+			await page.locator('body').fill("t");
 			await page.keyboard.up("Alt");
 			await page.locator('body').press("Enter");
 			expect(
@@ -535,12 +486,9 @@ test.describe('$Window Component', () => {
 				});
 				$window.$content.append('<p>Click in the blank space of the window</p>');
 			});
-			await page.locator(
-				'.window').FIXME_should('not.have.focus');
-			await page.locator(
-				'.window-content').click();
-			await page.locator(
-				'.window-content').FIXME_should('have.focus');
+			await page.locator('.window').FIXME_should('not.have.focus');
+			await page.locator('.window-content').click();
+			await page.locator('.window-content').FIXME_should('have.focus');
 
 		});
 		test("should focus the window when clicking on the title bar", async ({ page }) => {
@@ -550,12 +498,9 @@ test.describe('$Window Component', () => {
 				});
 				$window.$content.append('<p>Click on the title bar</p>');
 			});
-			await page.locator(
-				'.window').FIXME_should('not.have.focus');
-			await page.locator(
-				'.window-titlebar').click();
-			await page.locator(
-				'.window-content').FIXME_should('have.focus');
+			await page.locator('.window').FIXME_should('not.have.focus');
+			await page.locator('.window-titlebar').click();
+			await page.locator('.window-content').FIXME_should('have.focus');
 
 		});
 		test("should focus a control in the window when clicking it", async ({ page }) => {
@@ -565,22 +510,16 @@ test.describe('$Window Component', () => {
 				});
 				$window.$content.append('<form><input type="text" id="input" value="Click me"><textarea id="textarea">Text area</textarea></form>');
 			});
-			await page.locator(
-				'#input').FIXME_should('not.have.focus');
-			await page.locator(
-				'#input').click();
-			await page.locator(
-				'#input').FIXME_should('have.focus');
-			await page.locator(
-				'body').click();
-			await page.locator(
-				'#input').FIXME_should('not.have.focus');
+			await page.locator('#input').FIXME_should('not.have.focus');
+			await page.locator('#input').click();
+			await page.locator('#input').FIXME_should('have.focus');
+			await page.locator('body').click();
+			await page.locator('#input').FIXME_should('not.have.focus');
 			expect(document.activeElement).toBe(
 				document.body);
 			// refocusing logic should not override clicking a specific control
 			await page.locator('#textarea').click();
-			await page.locator(
-				'#textarea').FIXME_should('have.focus');
+			await page.locator('#textarea').FIXME_should('have.focus');
 
 		});
 		// I think Cypress's focus simulation logic breaks these tests.
@@ -602,22 +541,17 @@ test.describe('$Window Component', () => {
 			await page.locator('#disabled-button').dispatchEvent('pointerdown', { which: 1 });
 			// cy.get('.window-content').should('have.focus');
 			expect(document.activeElement).toBe(document.querySelector('.window-content'));
-			await page.locator(
-				'#enabled-button').click();
-			await page.locator(
-				'#enabled-button').FIXME_should('have.focus');
+			await page.locator('#enabled-button').click();
+			await page.locator('#enabled-button').FIXME_should('have.focus');
 			// cy.get('#disabled-button').click({ force: true });
 			await page.locator('#disabled-button').dispatchEvent('pointerdown', { which: 1 });
-			await page.locator(
-				'#enabled-button').FIXME_should('have.focus');
-			await page.locator(
-				'body').click();
+			await page.locator('#enabled-button').FIXME_should('have.focus');
+			await page.locator('body').click();
 			expect(document.activeElement).toBe(
 				document.body);
 			// cy.get('#disabled-button').click({ force: true });
 			await page.locator('#disabled-button').dispatchEvent('pointerdown', { which: 1 });
-			await page.locator(
-				'#enabled-button').FIXME_should('have.focus');
+			await page.locator('#enabled-button').FIXME_should('have.focus');
 		});
 		test.skip("should focus the last focused control in the window when closing another window that was focused", async ({ page }) => {
 			await page.evaluate(() => {
@@ -626,20 +560,15 @@ test.describe('$Window Component', () => {
 				});
 				$window.$content.append('<p>Window originally having focus <textarea id="textarea">Text area</textarea></p>');
 			});
-			await page.locator(
-				'#textarea').focus();
-			await page.locator(
-				'#textarea').FIXME_should('have.focus');
+			await page.locator('#textarea').focus();
+			await page.locator('#textarea').FIXME_should('have.focus');
 			const $window2 = $Window({
 				title: 'Popup Window'
 			});
 			$window2.$content.append('<p>Window taking focus temporarily</p><p><button id="close-popup">Close</button></p>');
-			await page.locator(
-				'#close-popup').focus();
-			await page.locator(
-				'#close-popup').FIXME_should('have.focus');
-			await page.locator(
-				'.window-close-button').last().click();
+			await page.locator('#close-popup').focus();
+			await page.locator('#close-popup').FIXME_should('have.focus');
+			await page.locator('.window-close-button').last().click();
 			// cy.get('#textarea').should('have.focus');
 			expect(document.activeElement).toBe(document.getElementById('textarea'));
 		});
