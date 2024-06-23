7e5d249382b541332ad9960cac56be60b6c52b0b
Isaiah Odhner <isaiahodhner@gmail.com>
2024-06-23 18:47:59 -0400

    [meta/ts] Add verbose typings for PostCSS config

--- postcss.config.js
+++ postcss.config.js
@@ -1,4 +1,41 @@
-module.exports = ctx => ({
+/**
+ * @typedef {('development'|'production'|string)} PostCSSEnv
+ */
+
+/**
+ * @typedef {Object} PostCSSFile
+ * @property {string} dirname
+ * @property {string} basename
+ * @property {string} extname
+ */
+
+/**
+ * @typedef {Object} PostCSSOptions
+ * @property {unknown} [map]
+ * @property {unknown} [parser]
+ * @property {unknown} [syntax]
+ * @property {unknown} [stringifier]
+ */
+
+/**
+ * @typedef {Object} PostCSSContext
+ * @property {PostCSSEnv} env
+ * @property {PostCSSFile} file
+ * @property {PostCSSOptions} options
+ */
+
+/**
+ * @typedef {Object} PostCSSConfig
+ * @property {unknown} [map]
+ * @property {unknown} [parser]
+ * @property {Record<string, unknown>} plugins
+ */
+
+/**
+ * @param {PostCSSContext} ctx
+ * @returns {PostCSSConfig} PostCSS configuration for a given file
+ */
+module.exports = (ctx) => ({
 	map: ctx.options.map,
 	parser: ctx.options.parser,
 	plugins: {
